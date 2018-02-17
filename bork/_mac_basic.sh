7682dd7d3cd6ed938357b672ea501ede25115e4f
Skylar MacDonald <skylar@skylarmacdonald.com>
2018-02-17 10:03:33 -0500

    Added more basics apps

--- bork/_mac_basic.sh
+++ bork/_mac_basic.sh
@@ -6,3 +6,8 @@ ok cask the-unarchiver
 ok cask google-chrome
 ok cask dropbox
 ok cask caffeine
+ok cask karabiner-elements
+ok mas 585829637 Todoist
+ok mas 457622435 Yoink
+ok mas 413965349 Soulver
+ok mas 406056744 Evernote
