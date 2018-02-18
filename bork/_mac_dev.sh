d48e320b89c91f38dcfa1c022b9abbb3042ba9cb
Skylar MacDonald <skylar@skylarmacdonald.com>
2018-02-18 16:36:09 -0500

    Added Ruby auto-install?

--- bork/_mac_dev.sh
+++ bork/_mac_dev.sh
@@ -6,3 +6,11 @@ ok brew ruby-install
 ok brew chruby
 ok mas 497799835 Xcode
 ok cask transmit
+
+# This may not work...
+if [ ! -d $HOME/.rubies ]; then
+	ok check "ruby-install ruby $(cat ~/.ruby-version)"
+fi
+ok check "source /usr/local/opt/chruby/share/chruby/chruby.sh"
+ok check "source /usr/local/opt/chruby/share/chruby/auto.sh"
+ok gem bundler
