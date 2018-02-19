4430da0a3064c1c1d56b7f53ca4499349b0a7ecc
Skylar MacDonald <skylar@skylarmacdonald.com>
2018-02-18 22:13:42 -0500

    Put all the Ruby stuff together

--- bork/_mac_dev.sh
+++ bork/_mac_dev.sh
@@ -2,12 +2,12 @@ ok cask sublime-text
 ok cask dash
 ok brew mysql
 ok check "brew services start mysql"
-ok brew ruby-install
-ok brew chruby
 ok mas 497799835 Xcode
 ok cask transmit
 ok cask sequel-pro
 
+ok brew ruby-install
+ok brew chruby
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
 ok check "chruby $(cat ~/.ruby-version)"
