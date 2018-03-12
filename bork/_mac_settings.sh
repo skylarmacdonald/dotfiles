66a08609f90261c40675ff360f9203ec86dd7fdc
Skylar MacDonald <skylar@skylarmacdonald.com>
2018-03-11 23:45:56 -0400

    Added some tweaks I stole from @mathiasbynens

--- bork/_mac_settings.sh
+++ bork/_mac_settings.sh
@@ -5,11 +5,22 @@ ok defaults NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 bool true
 # auto-expand print boxes
 ok defaults NSGlobalDomain PMPrintingExpandedStateForPrint bool true
 ok defaults NSGlobalDomain PMPrintingExpandedStateForPrint2 bool true
+# Save to disk (not to iCloud) by default
+ok defaults NSGlobalDomain NSDocumentSaveNewDocumentsToCloud bool false
+# Automatically quit printer app once the print jobs complete
+ok defaults com.apple.print.PrintingPrefs "Quit When Finished" bool true
+# Disable the “Are you sure you want to open this application?” dialog
+ok defaults com.apple.LaunchServices LSQuarantine bool false
+# Disable Resume system-wide
+ok defaults com.apple.systempreferences NSQuitAlwaysKeepsWindows bool false
 # do not ask to use new disks for Time Machine
 ok defaults com.apple.TimeMachine DoNotOfferNewDisksForBackup bool true
 # ask for password immediately after starting screensaver or sleep
 ok defaults com.apple.screensaver askForPassword -int 1
 ok defaults com.apple.screensaver askForPasswordDelay -int 0
+
+# Disable Notification Center and remove the menu bar icon
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
 include _mac_settings_dock.sh
 include _mac_settings_finder.sh
