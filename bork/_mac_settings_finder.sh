02f9ae27aca536b635b11fcbb3ff13c072c79fcd
Skylar MacDonald <skylar@skylarmacdonald.com>
2021-02-09 14:16:49 +0000

    More Finder tweaks

--- bork/_mac_settings_finder.sh
+++ bork/_mac_settings_finder.sh
@@ -6,9 +6,9 @@ ok defaults com.apple.finder NewWindowTarget string "PfHm"
 ok defaults com.apple.finder NewWindowTargetPath string "file://${HOME}/"
 
 # Show icons for hard drives, servers, and removable media on the desktop
-ok defaults com.apple.finder ShowExternalHardDrivesOnDesktop bool true
+ok defaults com.apple.finder ShowExternalHardDrivesOnDesktop bool false
 ok defaults com.apple.finder ShowHardDrivesOnDesktop bool true
-ok defaults com.apple.finder ShowMountedServersOnDesktop bool true
+ok defaults com.apple.finder ShowMountedServersOnDesktop bool false
 ok defaults com.apple.finder ShowRemovableMediaOnDesktop bool true
 
 # When performing a search, search the current folder by default
@@ -31,16 +31,23 @@ ok defaults com.apple.frameworks.diskimages auto-open-ro-root bool true
 ok defaults com.apple.frameworks.diskimages auto-open-rw-root bool true
 ok defaults com.apple.finder OpenWindowForNewRemovableDisk bool true
 
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+# Use icon view in all Finder windows by default
 ok defaults com.apple.finder FXPreferredViewStyle string "icnv"
 
 # Disable the warning before emptying the Trash
 ok defaults com.apple.finder WarnOnEmptyTrash bool false
 
+# Enable snap-to-grid for icons on the desktop and in other icon views
+if satisfying; then
+	/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+	/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+	/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+fi
+
 # Expand the following File Info panes:
 # “General”, “Open with”, and “Sharing & Permissions”
-ok defaults com.apple.finder FXInfoPanesExpanded dict General -bool true OpenWith -bool true Privileges -bool true
+# Does not use Bork syntax as difficult to do with a dict
+defaults write com.apple.finder FXInfoPanesExpanded -dict General -bool true OpenWith -bool true Privileges -bool true
 
 if any_updated; then
   killall Finder
